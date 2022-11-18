a62157587ecdc7432dad64004a4c580825e58a7f
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-18 16:32:41 +0100

    feat: App emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
+import { css } from '@emotion/react';
 
 const App: React.FC = () => {
     return (
-        <div>
-            <h1>main</h1>
+        <div css={containerStyles}>
+            <h1 css={headerStyles}>LOA Tracker</h1>
         </div>
     );
 };
 
 export default App;
+
+const containerStyles = css`
+    font-family: 'Roboto';
+`;
+
+const headerStyles = css`
+    margin: 0;
+`;
