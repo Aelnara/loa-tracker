5bd1c1ff581565d773485bb15ce0d4fed70e29c8
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-23 12:02:12 +0100

    feat: App Main

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { css } from '@emotion/react';
 import Nav from './sections/Nav/Nav';
 import { NavProvider } from './contexts/NavContext';
+import Main from './sections/Main/Main';
 
 const App: React.FC = () => {
     return (
         <NavProvider>
             <div css={containerStyles}>
                 <Nav />
+                <Main />
             </div>
         </NavProvider>
     );
