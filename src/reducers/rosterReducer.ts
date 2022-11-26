9e8c9854fd0984c6db5f3f09c926859ea94ac81b
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-26 14:56:00 +0100

    feat: rosterReducer update progress

--- src/reducers/rosterReducer.ts
+++ src/reducers/rosterReducer.ts
@@ -1,4 +1,5 @@
 import { Character } from '../types/Character';
+import { Progress } from '../types/Progress';
 
 type AddCharAction = {
     type: 'ADD_CHAR';
@@ -20,22 +21,40 @@ type ReorderRosterAction = {
     payload: Character[];
 };
 
-export type RosterAction = AddCharAction | RemoveCharAction | UpdateCharAction | ReorderRosterAction;
+type UpdateProgressAction = {
+    type: 'UPDATE_PROGRESS';
+    payload: {
+        id: string;
+        progress: Partial<Progress>;
+    };
+};
+
+export type RosterAction = AddCharAction | RemoveCharAction | UpdateCharAction | ReorderRosterAction | UpdateProgressAction;
 
 const rosterReducer = (state: Character[], action: RosterAction): Character[] => {
+    const updateCharacter = (id: string, updatedChar: Character) => {
+        const updatedRoster: Character[] = state;
+        const idx = updatedRoster.findIndex((char) => char.id === id);
+        updatedRoster.splice(idx, 1, updatedChar);
+
+        return [...updatedRoster];
+    };
+
     switch (action.type) {
         case 'ADD_CHAR':
             return [...state, action.payload];
         case 'REMOVE_CHAR':
             return state.filter((char) => char.id !== action.payload);
         case 'UPDATE_CHAR':
-            const updatedRoster: Character[] = state;
-            const idx = updatedRoster.findIndex((char) => char.id === action.payload.id);
-            updatedRoster.splice(idx, 1, action.payload);
-
-            return updatedRoster;
+            return updateCharacter(action.payload.id, action.payload);
         case 'REORDER_ROSTER':
             return action.payload;
+        case 'UPDATE_PROGRESS':
+            let updatedProgressRoster = state;
+            const charToUpdate = state.find((char) => char.id === action.payload.id);
+            if (charToUpdate) updatedProgressRoster = updateCharacter(action.payload.id, { ...charToUpdate, progress: { ...charToUpdate.progress, ...action.payload.progress } });
+
+            return updatedProgressRoster;
         default:
             return state;
     }
