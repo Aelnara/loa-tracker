d3868d6041df4454a759051e53303097632db7c4
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-28 15:54:52 +0100

    feat: GoldProgression text color

--- src/sections/Nav/GoldProgression.tsx
+++ src/sections/Nav/GoldProgression.tsx
@@ -4,6 +4,7 @@ import { css } from '@emotion/react';
 import { RosterContext, RosterContextType } from '../../contexts/RosterContext';
 import gold from '../../assets/icons/gold.png';
 import { getGoldForRoster, getPossibleRawGoldForRoster } from '../../utils/goldProgression';
+import Colors from '../../constants/Colors';
 
 const GoldProgression: React.FC = () => {
     const { roster } = useContext(RosterContext) as RosterContextType;
@@ -26,6 +27,7 @@ const containerStyles = css`
 const goldLabelStyles = css`
     font-size: 20px;
     letter-spacing: 1px;
+    color: ${Colors.GOLD};
 `;
 
 const goldLogoStyles = css`
