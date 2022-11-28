c8087685e2feaaed53a82f934ee6d8349d1f9632
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-28 10:51:25 +0100

    feat: rosterReducer reseting progress

--- src/reducers/rosterReducer.ts
+++ src/reducers/rosterReducer.ts
@@ -29,7 +29,12 @@ type UpdateProgressAction = {
     };
 };
 
-export type RosterAction = AddCharAction | RemoveCharAction | UpdateCharAction | ReorderRosterAction | UpdateProgressAction;
+type ResetProgressAction = {
+    type: 'RESET_PROGRESS';
+    payload: 'daily' | 'weekly' | 'full';
+};
+
+export type RosterAction = AddCharAction | RemoveCharAction | UpdateCharAction | ReorderRosterAction | UpdateProgressAction | ResetProgressAction;
 
 const rosterReducer = (state: Character[], action: RosterAction): Character[] => {
     const updateCharacter = (id: string, updatedChar: Character) => {
@@ -42,6 +47,13 @@ const rosterReducer = (state: Character[], action: RosterAction): Character[] =>
 
     const saveToStorage = (roster: Character[]) => {
         localStorage.setItem('loa-tracker-progress', JSON.stringify(roster));
+    };
+
+    const resetProgress = (progress: Progress, resetType: 'daily' | 'weekly' | 'full'): Progress => {
+        if (resetType === 'daily') return { ...progress, daily_una: false, chaos_dungeon: false, guardian_raid: false, guild_contribution: false };
+        if (resetType === 'weekly') return { ...progress, weekly_una: false, argos: false, valtan: false, vykas: false, kakul: false };
+        if (resetType === 'full') return { ...progress, daily_una: false, chaos_dungeon: false, guardian_raid: false, guild_contribution: false, weekly_una: false, argos: false, valtan: false, vykas: false, kakul: false };
+        return progress;
     };
 
     switch (action.type) {
@@ -66,6 +78,10 @@ const rosterReducer = (state: Character[], action: RosterAction): Character[] =>
             if (charToUpdate) rosterWithUpdatedProgress = updateCharacter(action.payload.id, { ...charToUpdate, progress: { ...charToUpdate.progress, ...action.payload.progress } });
             saveToStorage(rosterWithUpdatedProgress);
             return rosterWithUpdatedProgress;
+        case 'RESET_PROGRESS':
+            const rosterWithResetProgress = state.map((char) => ({ ...char, progress: resetProgress(char.progress, action.payload) }));
+            saveToStorage(rosterWithResetProgress);
+            return rosterWithResetProgress;
         default:
             saveToStorage(state);
             return state;
