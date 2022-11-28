dd8426697e462e3ee966958fbe509a17bdf24a18
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-28 14:11:29 +0100

    feat: created GoldProgression

---
+++ src/sections/Nav/GoldProgression.tsx
@@ -0,0 +1,37 @@
+/** @jsxImportSource @emotion/react */
+import React, { useContext } from 'react';
+import { css } from '@emotion/react';
+import { RosterContext, RosterContextType } from '../../contexts/RosterContext';
+import gold from '../../assets/icons/gold.png';
+import { getGoldForRoster, getPossibleRawGoldForRoster } from '../../utils/goldProgression';
+
+const GoldProgression: React.FC = () => {
+    const { roster } = useContext(RosterContext) as RosterContextType;
+
+    return (
+        <div css={containerStyles}>
+            <p css={goldLabelStyles}>
+                {getGoldForRoster(roster).toLocaleString()} / {getPossibleRawGoldForRoster(roster).toLocaleString()}
+            </p>
+            <img css={goldLogoStyles} src={gold} alt="gold" />
+        </div>
+    );
+};
+
+const containerStyles = css`
+    display: flex;
+    align-items: center;
+`;
+
+const goldLabelStyles = css`
+    font-size: 20px;
+    letter-spacing: 1px;
+`;
+
+const goldLogoStyles = css`
+    width: 40px;
+    height: 40px;
+    margin-left: 12px;
+`;
+
+export default GoldProgression;
