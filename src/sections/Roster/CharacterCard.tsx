a06d02c46982a519837107e3ae34bb6eb3e43f1b
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-29 14:37:21 +0100

    feat: CharacterCard grab indicator

--- src/sections/Roster/CharacterCard.tsx
+++ src/sections/Roster/CharacterCard.tsx
@@ -39,7 +39,7 @@ const cardStyles = css`
     box-sizing: border-box;
     border-radius: 12px;
     filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5));
-    cursor: pointer;
+    cursor: grab;
 
     &:hover {
         transform: scale(1.01);
