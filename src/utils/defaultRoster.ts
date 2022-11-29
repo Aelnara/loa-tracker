503f745c5b74ba9b4b5ab8c1f091a0547cf7f24e
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-11-29 14:45:01 +0100

    chore: defaultRoster

---
+++ src/utils/defaultRoster.ts
@@ -0,0 +1,114 @@
+import { Character } from '../types/Character';
+
+const defaultRoster: Character[] = [
+    {
+        id: '1',
+        name: 'Aelnara',
+        ilvl: 1500,
+        class: 'Shadowhunter',
+        progress: {
+            daily_una: false,
+            chaos_dungeon: false,
+            guardian_raid: false,
+            guild_contribution: false,
+
+            weekly_una: false,
+            argos: false,
+            valtan: false,
+            vykas: false,
+            kakul: false,
+        },
+    },
+    {
+        id: '2',
+        name: 'Alariah',
+        ilvl: 1460,
+        class: 'Deathblade',
+        progress: {
+            daily_una: false,
+            chaos_dungeon: false,
+            guardian_raid: false,
+            guild_contribution: false,
+
+            weekly_una: false,
+            argos: false,
+            valtan: false,
+            vykas: false,
+            kakul: false,
+        },
+    },
+    {
+        id: '3',
+        name: 'Alyressa',
+        ilvl: 1460,
+        class: 'Reaper',
+        progress: {
+            daily_una: false,
+            chaos_dungeon: false,
+            guardian_raid: false,
+            guild_contribution: false,
+
+            weekly_una: false,
+            argos: false,
+            valtan: false,
+            vykas: false,
+            kakul: false,
+        },
+    },
+    {
+        id: '4',
+        name: 'Adyriah',
+        ilvl: 1460,
+        class: 'Sorceress',
+        progress: {
+            daily_una: false,
+            chaos_dungeon: false,
+            guardian_raid: false,
+            guild_contribution: false,
+
+            weekly_una: false,
+            argos: false,
+            valtan: false,
+            vykas: false,
+            kakul: false,
+        },
+    },
+    {
+        id: '5',
+        name: 'Alaressa',
+        ilvl: 1460,
+        class: 'Gunslinger',
+        progress: {
+            daily_una: false,
+            chaos_dungeon: false,
+            guardian_raid: false,
+            guild_contribution: false,
+
+            weekly_una: false,
+            argos: false,
+            valtan: false,
+            vykas: false,
+            kakul: false,
+        },
+    },
+    {
+        id: '6',
+        name: 'Adyressa',
+        ilvl: 1460,
+        class: 'Glaivier',
+        progress: {
+            daily_una: false,
+            chaos_dungeon: false,
+            guardian_raid: false,
+            guild_contribution: false,
+
+            weekly_una: false,
+            argos: false,
+            valtan: false,
+            vykas: false,
+            kakul: false,
+        },
+    },
+];
+
+export default defaultRoster;
