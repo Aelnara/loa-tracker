e986961c67f82670b14df2aaf66abfd1a1c81cdf
Aelnara <58818736+Aelnara@users.noreply.github.com>
2022-12-05 10:50:46 +0100

    refactor: defaultRoster

--- src/utils/defaultRoster.ts
+++ src/utils/defaultRoster.ts
@@ -1,10 +1,11 @@
 import { Character } from '../types/Character';
+import { v4 } from 'uuid';
 
 const defaultRoster: Character[] = [
     {
-        id: '1',
-        name: 'Aelnara',
-        ilvl: 1500,
+        id: v4(),
+        name: 'Character 1',
+        ilvl: 1490,
         class: 'Shadowhunter',
         gold_earning: true,
         progress: {
@@ -21,9 +22,9 @@ const defaultRoster: Character[] = [
         },
     },
     {
-        id: '2',
-        name: 'Alariah',
-        ilvl: 1460,
+        id: v4(),
+        name: 'Character 2',
+        ilvl: 1445,
         class: 'Deathblade',
         gold_earning: true,
         progress: {
@@ -40,67 +41,10 @@ const defaultRoster: Character[] = [
         },
     },
     {
-        id: '3',
-        name: 'Alyressa',
-        ilvl: 1460,
+        id: v4(),
+        name: 'Character 3',
+        ilvl: 1445,
         class: 'Reaper',
-        gold_earning: true,
-        progress: {
-            daily_una: false,
-            chaos_dungeon: false,
-            guardian_raid: false,
-            guild_contribution: false,
-
-            weekly_una: false,
-            argos: false,
-            valtan: false,
-            vykas: false,
-            kakul: false,
-        },
-    },
-    {
-        id: '4',
-        name: 'Adyriah',
-        ilvl: 1460,
-        class: 'Sorceress',
-        gold_earning: true,
-        progress: {
-            daily_una: false,
-            chaos_dungeon: false,
-            guardian_raid: false,
-            guild_contribution: false,
-
-            weekly_una: false,
-            argos: false,
-            valtan: false,
-            vykas: false,
-            kakul: false,
-        },
-    },
-    {
-        id: '5',
-        name: 'Alaressa',
-        ilvl: 1460,
-        class: 'Gunslinger',
-        gold_earning: true,
-        progress: {
-            daily_una: false,
-            chaos_dungeon: false,
-            guardian_raid: false,
-            guild_contribution: false,
-
-            weekly_una: false,
-            argos: false,
-            valtan: false,
-            vykas: false,
-            kakul: false,
-        },
-    },
-    {
-        id: '6',
-        name: 'Adyressa',
-        ilvl: 1460,
-        class: 'Glaivier',
         gold_earning: true,
         progress: {
             daily_una: false,
